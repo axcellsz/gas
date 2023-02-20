6cef39510fa20ca94b3f6f3c1ab911330c93fbed
axcellsz <122135545+axcellsz@users.noreply.github.com>
2023-02-20 18:53:08 +0700

    Update ups.sh

--- ups.sh
+++ ups.sh
@@ -2,9 +2,9 @@
 clear
 # update
 
-# rm -rf /usr/bin/vms-flx
-# wget -O /usr/bin/vms-flx https://github.com/axcellsz/gas/raw/main/up/vms-flx && chmod +x /usr/bin/vms-flx
-wget -O /usr/bin/menu https://github.com/axcellsz/gas/raw/main/up/menu && chmod +x /usr/bin/menu
+rm -rf /usr/bin/vms-flx
+wget -O /usr/bin/vms-flx https://github.com/axcellsz/gas/raw/main/up/vms-flx && chmod +x /usr/bin/vms-flx
+#wget -O /usr/bin/menu https://github.com/axcellsz/gas/raw/main/up/menu && chmod +x /usr/bin/menu
 
 
 # update
