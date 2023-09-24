3c6ca84bc29233df4f0d03f4d669e12e495e7d8e
axcellsz <122135545+axcellsz@users.noreply.github.com>
2023-09-24 19:19:22 +0700

    Update ups.sh

--- ups.sh
+++ ups.sh
@@ -4,10 +4,11 @@ clear
 
 #rm -rf /usr/bin/add-vidio
 #rm -rf /usr/bin/menu
-wget -O /usr/bin/add-vidio https://github.com/axcellsz/gas/raw/main/up/add-vidio && chmod +x /usr/bin/add-vidio
+#wget -O /usr/bin/add-vidio https://github.com/axcellsz/gas/raw/main/up/add-vidio && chmod +x /usr/bin/add-vidio
 #wget -O /usr/bin/menu https://github.com/axcellsz/gas/raw/main/up2/menu && chmod +x /usr/bin/menu
 #rm -rf /usr/bin/vms-def
-#wget -O /usr/bin/vms-def https://github.com/axcellsz/gas/raw/main/up2/vms-def && chmod +x /usr/bin/vms-def
+wget -O /usr/bin/vms-def https://github.com/axcellsz/gas/raw/main/up2/vms-def && chmod +x /usr/bin/vms-def
+wget -O /usr/bin/vms-flx https://github.com/axcellsz/gas/raw/main/up2/vms-flx && chmod +x /usr/bin/vms-flx
 
 
 # update
