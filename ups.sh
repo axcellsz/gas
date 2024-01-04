af593866331eb731349b2362913be2e1bc5920aa
axcellsz <122135545+axcellsz@users.noreply.github.com>
2024-01-04 17:38:27 +0700

    Update ups.sh

--- ups.sh
+++ ups.sh
@@ -3,9 +3,21 @@
 #     wget -O /var/www/html/create_account.php "https://github.com/axcellsz/gas/raw/main/file/xray/create_account.php"
 #     chmod 777 /var/www/html/create_account.php
 
-     rm /usr/bin/menu
-     wget -O /usr/bin/menu "https://github.com/axcellsz/gas/raw/main/file/xray/menu"
-     chmod 777 /usr/bin/menu
+#     rm /usr/bin/menu
+#     wget -O /usr/bin/menu "https://github.com/axcellsz/gas/raw/main/file/xray/menu"
+#     chmod 777 /usr/bin/menu
+
+      wget -O /var/www/html/web-default "https://github.com/axcellsz/gas/raw/main/file/xray/web-default"
+      chmod 777 /var/www/html/web-default
+
+      wget -O /var/www/html/web-flex "https://github.com/axcellsz/gas/raw/main/file/xray/web-flex"
+      chmod 777 /var/www/html/web-flex
+
+      wget -O /var/www/html/web-vidio "https://github.com/axcellsz/gas/raw/main/file/xray/web-vidio"
+      chmod 777 /var/www/html/web-vidio
+
+      wget -O /var/www/html/web-edu "https://github.com/axcellsz/gas/raw/main/file/xray/web-edu"
+      chmod 777 /var/www/html/web-edu
     
 systemctl daemon-reload
 systemctl restart nginx
