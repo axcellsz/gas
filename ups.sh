304603a6e7d66233a4c63e3713eaae42277a9eeb
axcellsz <122135545+axcellsz@users.noreply.github.com>
2024-01-04 10:06:37 +0700

    Update ups.sh

--- ups.sh
+++ ups.sh
@@ -1,8 +1,12 @@
 !#/bin/bash
-     rm /var/www/html/create_account.php
-     wget -O /var/www/html/create_account.php "https://github.com/axcellsz/gas/raw/main/file/xray/create_account.php"
-     chmod 777 /var/www/html/create_account.php
-     
+#     rm /var/www/html/create_account.php
+#     wget -O /var/www/html/create_account.php "https://github.com/axcellsz/gas/raw/main/file/xray/create_account.php"
+#     chmod 777 /var/www/html/create_account.php
+
+     rm /usr/bin/menu
+     wget -O /usr/bin/menu "https://github.com/axcellsz/gas/raw/main/file/xray/menu"
+     chmod 777 /usr/bin/menu
+    
 systemctl daemon-reload
 systemctl restart nginx
 systemctl restart xray
