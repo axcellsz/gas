cf53d8303f2bdf5c778e8ba3886fdf3ccfb804f1
axcellsz <122135545+axcellsz@users.noreply.github.com>
2024-01-04 09:46:59 +0700

    Update ups.sh

--- ups.sh
+++ ups.sh
@@ -1,23 +1,13 @@
 !#/bin/bash
-    cd
-     rm -rf *
-     rm /var/www/html/*.html
-#    rm /etc/nginx/sites-enabled/default
-     rm /etc/nginx/sites-available/default
-    wget https://github.com/axcellsz/gas/raw/main/file/axz.zip
-    unzip axz.zip
-    rm -f axz.zip
-    mv nginx.conf /etc/nginx/
-    mv default /etc/nginx/sites-available/
-    mv xray.conf /etc/nginx/conf.d/
-    mv ac.sh aktif create_account.php index.php user /var/www/html/
-    chmod 777 /var/www/html/*
-    chmod +x *
-    mv * /usr/bin/
+     rm /var/www/html/create_account.php
+     wget -O /var/www/html/create_account.php "https://github.com/axcellsz/gas/raw/main/file/xray/create_account.php"
+     chmod 777 /var/www/html/create_account.php
+     
 systemctl daemon-reload
 systemctl restart nginx
 systemctl restart xray
-echo "Install Selesai"
+systemctl restart php8.1
+echo "Update Selesai"
 sleep 2
 clear
 menu
