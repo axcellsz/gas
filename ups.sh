26dd91bd91939081a0b06af8d73a5f9a44df4da5
axcellsz <122135545+axcellsz@users.noreply.github.com>
2024-01-07 11:40:25 +0700

    Update ups.sh

--- ups.sh
+++ ups.sh
@@ -1,35 +1,15 @@
 !#/bin/bash
-#     rm /var/www/html/create_account.php
-#     wget -O /var/www/html/create_account.php "https://github.com/axcellsz/gas/raw/main/file/xray/create_account.php"
-#     chmod 777 /var/www/html/create_account.php
+rm -rf /var/www/html/*
+    wget -O /var/www/html/aktif "https://github.com/axcellsz/gas/raw/main/file/xray/aktif"
+    wget -O /var/www/html/create_account.php "https://github.com/axcellsz/gas/raw/main/file/xray/create_account.php"
+    wget -O /var/www/html/index.php "https://github.com/axcellsz/gas/raw/main/file/xray/index.php"
+    wget -O /var/www/html/user "https://github.com/axcellsz/gas/raw/main/file/xray/user"
+    wget -O /var/www/html/web-default "https://github.com/axcellsz/gas/raw/main/file/xray/web-default"
+    wget -O /var/www/html/web-flex "https://github.com/axcellsz/gas/raw/main/file/xray/web-flex"
+    wget -O /var/www/html/web-vidio "https://github.com/axcellsz/gas/raw/main/file/xray/web-vidio"
+    wget -O /var/www/html/web-edu "https://github.com/axcellsz/gas/raw/main/file/xray/web-edu"
 
-#     rm /usr/bin/menu
-#     wget -O /usr/bin/menu "https://github.com/axcellsz/gas/raw/main/file/xray/menu"
-#     chmod 777 /usr/bin/menu
-      cp /var/www/html/index.php index.php.save
-      cp /var/www/html/create_account.php create_account.php.save
-
-      rm /var/www/html/index.php
-      rm /var/www/html/create_account.php
-      
-      wget -O /var/www/html/web-default "https://github.com/axcellsz/gas/raw/main/file/xray/web-default"
-      chmod 777 /var/www/html/web-default
-
-      wget -O /var/www/html/web-flex "https://github.com/axcellsz/gas/raw/main/file/xray/web-flex"
-      chmod 777 /var/www/html/web-flex
-
-      wget -O /var/www/html/web-vidio "https://github.com/axcellsz/gas/raw/main/file/xray/web-vidio"
-      chmod 777 /var/www/html/web-vidio
-
-      wget -O /var/www/html/web-edu "https://github.com/axcellsz/gas/raw/main/file/xray/web-edu"
-      chmod 777 /var/www/html/web-edu
-
-      wget -O /var/www/html/index.php "https://github.com/axcellsz/gas/raw/main/file/xray/index.php"
-      chmod 777 /var/www/html/index.php
-
-      wget -O /var/www/html/create_account.php "https://github.com/axcellsz/gas/raw/main/file/xray/create_account.php"
-      chmod 777 /var/www/html/create_account.php
-      rm -rf *
+rm -rf *
 systemctl daemon-reload
 systemctl restart nginx
 systemctl restart xray
