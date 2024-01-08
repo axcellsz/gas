bc4d34710e61bed227ad63b01ec6978631c57e03
axcellsz <122135545+axcellsz@users.noreply.github.com>
2024-01-08 11:40:47 +0700

    Update ups.sh

--- ups.sh
+++ ups.sh
@@ -1,20 +1,3 @@
 !#/bin/bash
-rm -rf /var/www/html/*
-    wget -O /var/www/html/aktif "https://github.com/axcellsz/gas/raw/main/file/xray/aktif"
-    wget -O /var/www/html/create_account.php "https://github.com/axcellsz/gas/raw/main/file/xray/create_account.php"
-    wget -O /var/www/html/index.php "https://github.com/axcellsz/gas/raw/main/file/xray/index.php"
-    wget -O /var/www/html/user "https://github.com/axcellsz/gas/raw/main/file/xray/user"
-    wget -O /var/www/html/web-default "https://github.com/axcellsz/gas/raw/main/file/xray/web-default"
-    wget -O /var/www/html/web-flex "https://github.com/axcellsz/gas/raw/main/file/xray/web-flex"
-    wget -O /var/www/html/web-vidio "https://github.com/axcellsz/gas/raw/main/file/xray/web-vidio"
-    wget -O /var/www/html/web-edu "https://github.com/axcellsz/gas/raw/main/file/xray/web-edu"
-chmod 777 /var/www/html/*
-rm -rf *
-systemctl daemon-reload
-systemctl restart nginx
-systemctl restart xray
-systemctl restart php8.1-fpm
-echo "Update Selesai"
-sleep 2
-clear
-menu
+    wget -O /usr/bin/cek "https://github.com/axcellsz/gas/raw/main/file/xray/cek"
+    chmod +x /usr/bin/cek
