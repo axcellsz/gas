1be5ed937bb0c6c87d4b863444f256264fcdfd95
axcellsz <122135545+axcellsz@users.noreply.github.com>
2024-01-27 12:50:52 +0700

    Update curl.php

--- file/project-v/curl.php
+++ file/project-v/curl.php
@@ -1,20 +1,19 @@
 <?php
 if(isset($_POST['create'])){
-$user = $_POST["user"];
-$aktif = $_POST["aktif"];
-$package = $_POST["paket"];
+$user= $_POST['user'];
+$aktif= $_POST['aktif'];
+$package= $_POST['paket'];
 
-$url = 'http://157.230.37.167/create_account.php';
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$data = "user=$user&aktif=$aktif&package=$package";
-curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-$result = curl_exec($curl);
-curl_close($curl);
+$ch = curl_init('http://157.230.37.167/create_account.php');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$data = "user=$user&aktif=$aktif&paket=$package";
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+$resp = curl_exec($ch);
+echo $resp;
+curl_close($ch);
+}
 ?>
-
-
 
 <!DOCTYPE html>
 <html>
