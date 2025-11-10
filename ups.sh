824d51be4fc061aac990cc6d30d7fea9900bba94
axcellsz <122135545+axcellsz@users.noreply.github.com>
2025-11-11 02:00:47 +0700

    Update ups.sh

--- ups.sh
+++ ups.sh
@@ -1,3 +1,3 @@
 !#/bin/bash
-    wget -O /etc/nginx/conf.d/xray.conf "https://github.com/axcellsz/gas/raw/main/up/nginx.conf"
-    chmod +x /etc/nginx/conf.d/xray.conf
+    wget -O /etc/nginx/nginx.conf "https://github.com/axcellsz/gas/raw/main/up/nginx.conf"
+    chmod +x /etc/nginx/nginx.conf
